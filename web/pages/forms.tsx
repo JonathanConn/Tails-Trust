024a4dbe24006ab5df39c31af66f8cd3a8c6910d
Jonathan Conn <jonathanconn29@gmail.com>
2022-11-13 14:23:10 -0500

    fixed code reuse and simplified routing

--- web/pages/forms.tsx
+++ web/pages/forms.tsx
@@ -1,26 +1,94 @@
-import { Component } from "react";
 import { useRouter } from 'next/router'
-
-import BasicForm from "../components/forms/basicForm";
-import RevocableForm from "../components/forms/revocableForm";
-import TontineForm from "../components/forms/tontineForm";
-
+import { ethers } from 'ethers'
+import { useAccContext } from '../context/AccountProvider';
 
 export default function Forms({ children }: any) {
+
     const router = useRouter()
     const { formType } = router.query
 
-    if (formType === 'basic') {
-        return <BasicForm/>
-    }
-    else if (formType === 'revocable') {
-        return <RevocableForm/>
-    }
-    else if (formType === 'tontine') {
-        return <TontineForm/>
-    }
-    else {
-        return <div>Invalid form type</div>
+    let desc = '';
+    let input3 = (<input id="input3" type="date" className="input input-bordered input-lg" required />);
+    if (formType === "basic") {
+        desc = 'Distribution available after unlock'
+    } else if (formType === "revocable") {
+        desc = 'Beneficiary can withdrawl after unlock date.\nOwner can cancel at any time.'
+    } else if (formType === "tontine") {
+        desc = 'Beneficiary can withdrawl after owner passes away.'
+        input3 = (
+            <label className="input-group input-group-lg">
+                <span>Check in period</span>
+                <select id="input3" className="input input-bordered input-lg" placeholder="3600" required>
+                    <option value="3600">Hourly</option>
+                    <option value="86400">Daily</option>
+                    <option value="604800">Weekly</option>
+                    <option value="2592000">Monthly</option>
+                    <option value="31536000">Yearly</option>
+                </select>
+            </label>
+        )
     }
 
+
+    const provider = new ethers.providers.Web3Provider((window as any).ethereum, "any");
+    const abi = require('../public/TrustFactory.json').abi;
+
+    const create = async (event: any) => {
+        event.preventDefault();
+
+        const time = (formType === 'tontine') ?  event.target.input3.value as Number : new Date(event.target.input3.value).getTime();
+        const beneficiary = event.target.benInput.value as string;
+        const amount = ethers.utils.parseEther(event.target.amountInput.value);
+
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+
+        const factoryContract = new ethers.Contract(
+            process.env.NEXT_PUBLIC_CONTRACT_ADDRESS!,
+            abi as ethers.ContractInterface,
+            signer
+        );
+            
+        if (formType === "basic") {
+            factoryContract.createBasicTrust(beneficiary, time, { value: amount })
+        } else if (formType === "revocable") {
+            factoryContract.createRevokableTrust(beneficiary, time, { value: amount })
+        } else if (formType === "tontine") {
+            factoryContract.createTontineTrust(beneficiary, time,{ value: amount })
+        }
+
+      
+    }
+
+
+    return (
+        <div className="flex flex-col justify-center items-center text-center">
+
+            <a className="text-3xl font-bold my-10">Create a Trust</a>
+            <a className="text-md font-bold mb-10"> {desc} </a>
+
+            <form id="trustForm" onSubmit={create}>
+
+                <label className="input-group input-group-lg">
+                    <input id="benInput" type="text" placeholder="0x00" className="input input-bordered input-lg" required />
+                    <span>Inheritor</span>
+                </label>
+
+                <label className="input-group py-5 input-group-lg">
+                    <input id="amountInput" type="text" placeholder="0.01" className="input input-bordered input-lg" required />
+                    <span>ETH</span>
+                </label>
+
+
+                <label className="input-group input-group-lg">
+                    {input3}
+                </label>
+
+                <br></br>
+                <a className='btn'>Create</a>
+
+            </form>
+
+        </div>
+    )
 }
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,7 +1,32 @@
-import Menu from '../components/menu'
+import Head from 'next/head'
+import Link from 'next/link'
 
-export default function Home() {
-    return (
-        <Menu/>
-    )
+export default function Index() {
+  return (
+    <div>
+      <Head>
+        <title>Trust</title>
+      </Head>
+
+      <div className="container my-10 mockup-code">
+        <pre data-prefix="$"><code>Create Trust</code></pre>
+        <pre data-prefix=">" className="text-warning"><code>Minting...</code></pre>
+        <pre data-prefix=">" className="text-success"><code>Done!</code></pre>
+      </div>
+
+      <div className="container">
+        <div className="grid gap-3 grid-cols-3 px-5">
+
+          <Link className='btn' href={{ pathname: "/forms", query: { formType: "basic" } }}>Basic</Link>
+
+          <Link className='btn' href={{ pathname: "/forms", query: { formType: "revocable" } }}>Revocable</Link>
+
+          <Link className='btn' href={{ pathname: "/forms", query: { formType: "tontine" } }}>Tontine</Link>
+
+        </div>
+      </div>
+
+
+    </div>
+  )
 }
